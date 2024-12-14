d3ef4a2a33d96b2acb13010b212b0f4194d1b6d4
solace7289 <146842238+solace7289@users.noreply.github.com>
2024-12-14 22:30:23 +0700

    add: get attendances of batch on-progress

--- frontend/src/view/staff/schedule-management.vue
+++ frontend/src/view/staff/schedule-management.vue
@@ -743,7 +743,6 @@ export default {
         console.error("Session data not found for sessionId:", sessionId);
         return;
       }
-
       const updatedData = {
         date: sessionData.date,
         status: 0,
